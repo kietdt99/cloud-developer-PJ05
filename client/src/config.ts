9e8fcee791e27ecafa7ae87c26a4b7a1eadd7e21
Do Tuan Kiet <tuankiet141199@gmail.com>
2022-12-24 12:07:16 +0700

    update config FE

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,6 +3,6 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 
 export const authConfig = {
   domain: 'dev-1krxcqb4sfirg1i7.us.auth0.com',            // Auth0 domain
-  clientId: 'ugRQlGglyGIFlw9rAVU9srvwo89osgCw',          // Auth0 client id
+  clientId: 'EuG6gNSAQmcz2jr2NXii63D8ll2S1hIt',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
